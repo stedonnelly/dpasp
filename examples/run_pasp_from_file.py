5d80e6d130ea62e905fc013f959692152a9f1d46
Shaun Donnelly <shaun.t.e.donnelly@gmail.com>
2026-04-10 18:07:35 +0100

    Update documentation in run_pasp_from_file.py to clarify usage of quiet mode

--- examples/run_pasp_from_file.py
+++ examples/run_pasp_from_file.py
@@ -3,7 +3,13 @@ import pasp
 P = pasp.parse("simple.plp")
 
 output = P(status=True, verbose=True)
+"""
+Use
 
+P(quiet=True, verbose=True) 
+
+if you want to suppress the proabilities being printed to the terminal.
+"""
 # The result has a built-in string representation
 result_str = str(output)
 print(result_str)
